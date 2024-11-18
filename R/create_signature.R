6d6f5fea5966bd415c3b3b11380f7ee090533c73
Diana Wu <dianawu3214@hotmail.com>
2024-11-18 18:56:43 +0100

    Initial commit

---
+++ R/create_signature.R
@@ -0,0 +1,119 @@
+#' Format cv.glmnet Object from create_signature
+#'
+#' Extracts selected features from a `cv.glmnet` object using the lambda values for minimum error (`lambda.min`) and one standard error (`lambda.1se`).
+#'
+#' @param df_crossval A `cv.glmnet` object, typically obtained from `create_signature`.
+#'
+#' @return A list with two elements:
+#' \itemize{
+#'   \item \code{crossval}: The original `cv.glmnet` object.
+#'   \item \code{results}: A list with selected variables for `lambda.min` and `lambda.1se`.
+#' }
+#' 
+#'
+format_cvglmnet <- function(df_crossval){
+  
+  resultsCV_tmp <- list()
+  
+  resultsCV_tmp$lmin <- extract_features(df_crossval, "lambda.min")
+  
+  resultsCV_tmp$l1se <- extract_features(df_crossval, "lambda.1se")
+  
+  res <- list(df_crossval, resultsCV_tmp)
+  
+  names(res) <- c("crossval", "results")
+  return(res)
+}
+
+#' Extract Selected Features from a cv.glmnet Object at Specified Lambda
+#'
+#' This function extracts the selected features from a `cv.glmnet` object based on the coefficients at a specified lambda value. 
+#' It returns the names of the features that have non-zero coefficients, excluding the intercept.
+#'
+#' @param df_crossval A `cv.glmnet` object, typically the result of running `cv.glmnet` on a dataset. This object contains the cross-validation results and the fitted model.
+#' @param lambda A character string specifying the lambda value to use for feature selection. This can be either "lambda.min" or "lambda.1se" to select features based on the respective lambda value.
+#'
+#' @return A character vector containing the names of the features selected at the specified lambda, excluding the intercept.
+#'
+#' @details This function uses the `cv.glmnet` object to extract the coefficients at the specified lambda value. It identifies the features with non-zero coefficients and returns their names. The intercept is excluded from the result.
+#'
+#' @examples
+#' \dontrun{
+#' # Example usage of extract_features
+#' df_crossval <- glmnet::cv.glmnet(x = training_data, y = response_data)
+#' selected_features_min <- extract_features(df_crossval, "lambda.min")
+#' selected_features_1se <- extract_features(df_crossval, "lambda.1se")
+#' print(selected_features_min)
+#' print(selected_features_1se)
+#' }
+#'
+#' @seealso \code{\link[glmnet]{cv.glmnet}}, \code{\link{format_cvglmnet}}
+#' @export
+#'
+extract_features <- function(df_crossval, lambda){
+  coef_res <- predict(df_crossval$glmnet.fit, type = "coefficients", s = df_crossval[[lambda]])
+  
+  # Extract non-zero feature names at lambda
+  sig_features <- names(which(coef_res[, 1] != 0)[-1])  # Exclude intercept
+  return(sig_features)
+}
+
+#' Create Omics-Based Exposure Signature Using Cross-Validated LASSO
+#'
+#' Fits a cross-validated LASSO model using `cv.glmnet` to identify a signature of features associated with a specified outcome. Optionally supports parallel processing for faster computation.
+#'
+#' @param data A data frame containing both features and the outcome variable.
+#' @param train_idx A vector of row indices specifying the training subset of `data`.
+#' @param features A character vector of column names specifying the features (predictor variables) to be included in the model.
+#' @param outcome A character string specifying the name of the outcome variable (response) in `data`.
+#' @param folds Integer specifying the number of folds for cross-validation. Default is 5.
+#' @param parallel Logical value indicating whether to use parallel processing. Default is \code{FALSE}.
+#' @param cores Integer specifying the number of cores to use if \code{parallel = TRUE}. Default is 18.
+#'
+#' @return A list with two elements:
+#' \itemize{
+#'   \item \code{crossval}: The fitted `cv.glmnet` object.
+#'   \item \code{results}: A list of selected variables based on `lambda.min` and `lambda.1se`.
+#' }
+#' 
+#' @details This function performs LASSO regression using cross-validation to select optimal features. If \code{parallel = TRUE}, the function uses \code{doMC} for parallel computation with the specified number of cores.
+#'
+#' @examples
+#' \dontrun{
+#' data <- my_data_frame
+#' train_index <- sample(1:nrow(data), size = floor(0.8 * nrow(data)))
+#' feature_names <- colnames(data)[2:100] # Example feature names
+#' outcome_name <- "my_outcome"
+#' signature <- create_signature(data, train_index, feature_names, outcome_name, n_folds = 10, parallel = TRUE, n_cores = 4)
+#' }
+#'
+#' @seealso \code{\link[glmnet]{cv.glmnet}}, \code{\link[doMC]{registerDoMC}}, \code{\link{format_cvglmnet}}
+#' @export
+#'
+create_signature <- function(data, train_idx, features, exposure, covars = c(), folds = 5, parallel = F, cores = 18){
+  t1 <- Sys.time()
+  
+  # Feature/covariate dataframe
+  data_tmp <- data[train_idx, c(features, covars)] %>% as.matrix()
+  penalty_factors <- c(rep(1, length(features)), rep(0, length(covars)))
+  # Exposure dataframe
+  outcome_tmp <- data[train_idx, exposure] %>% as.matrix()
+  colnames(outcome_tmp) <- exposure
+  
+  # Start parallel instance if using parallelization
+  if(parallel){
+    doMC::registerDoMC(cores = cores)
+  }
+  crossval_tmp <- glmnet::cv.glmnet(x = data_tmp, y = outcome_tmp, 
+                                    lambda.min.ratio = 0.001,
+                                    type.measure = "mse", nlambda = 100, 
+                                    parallel = parallel, nfolds = folds,
+                                    penalty.factor = penalty_factors)
+  
+  res <- format_cvglmnet(crossval_tmp)
+  
+  t2 <- Sys.time()
+  cat("Cross-validated signature completed in", difftime(t2,t1), "\n")
+  
+  return(res)
+}
---
+++ R/predict_signature.R
@@ -0,0 +1,55 @@
+#' Predict Exposure Signatures from Cross-Validated Model
+#'
+#' This function uses a cross-validated model object (`cv.glmnet`) to predict exposure signatures for a test set. It returns the predicted values for both the `lambda.min` and `lambda.1se` values from the cross-validation.
+#'
+#' @param data A data frame or matrix containing the full set of features and exposure variable.
+#' @param crossval A `cv.glmnet` object resulting from cross-validation that contains the fitted model.
+#' @param test_idx A vector of indices specifying the set for which predictions are to be made.
+#' @param features A character vector or column indices specifying the feature names used for prediction.
+#' @param exposure A character string specifying the name of the exposure (response variable) in the `data` used for comparison with predictions.
+#'
+#' @return A list containing two matrices:
+#' \itemize{
+#'   \item \code{l1se}: A matrix with the actual exposure data and predicted signature using \code{lambda.1se}.
+#'   \item \code{lmin}: A matrix with the actual exposure data and predicted signature using \code{lambda.min}.
+#' }
+#' Each matrix has the exposure values in the first column and the predicted signature values in the second column.
+#'
+#' @details The function predicts exposure signatures using the fitted `cv.glmnet` model. Predictions are made for both the `lambda.1se` and `lambda.min` values. The returned matrices combine actual exposure values and predicted signatures for comparison.
+#' 
+#' @examples
+#' \dontrun{
+#' # Example usage
+#' data <- your_data_frame
+#' crossval <- cv.glmnet(x = data[train_idx, features], y = data[train_idx, exposure])
+#' test_idx <- sample(1:nrow(data), 50)  # Specify test set indices
+#' pred_results <- pred_signature(data, crossval, test_idx, features, exposure)
+#' }
+#'
+#' @seealso \code{\link[glmnet]{cv.glmnet}}
+#' @export
+#'
+pred_signature <- function(data, crossval, test_idx, features, exposure) {
+  # Ensure that crossval is a cv.glmnet object
+  if (!inherits(crossval, "cv.glmnet")) {
+    stop("The 'crossval' parameter must be a cv.glmnet object.")
+  }
+  
+  # Filter for test set + metabolomic features
+  data_tmp <- data[test_idx, features] %>% as.matrix()
+  
+  # Use fitted model to predict signature for both lambda.min and lambda.1se
+  pred_l1se <- predict(object = crossval, newx = data_tmp, s = crossval$lambda.1se)
+  pred_lmin <- predict(object = crossval, newx = data_tmp, s = crossval$lambda.min)
+  
+  # Get actual data for exposure from test set
+  actual_data <- data[test_idx, exposure]
+  
+  # Combine actual data and predicted signature values
+  pred_data <- list(
+    l1se = cbind(actual_data, predsig = pred_l1se) %>% as.matrix(),
+    lmin = cbind(actual_data, predsig = pred_lmin) %>% as.matrix()
+  )
+  
+  return(pred_data)
+}
